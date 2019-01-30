4779d71ffa581bc26a0b8588e85e7e9b46c6a28d
ramonqu <ramonqu@uw.edu>
2019-01-30 12:53:33 -0800

    add some connetes and easy JS file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
-  } 
+  }
+}
+
+function locateme(){
+
+  var elmnt = document.getElementById("map");
+  elmnt.scrollIntoView();
+}
+
+function search(){
+
 }
