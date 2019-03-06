66995d41a5acd80c99bb6258332c3c41c5569e4f
ramonqu <ramonqu@uw.edu>
2019-03-06 09:20:34 -0800

    Updates

---
+++ precache-manifest.13038383a3fe612dc30d9c1ac3250076.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4ba9cb1b9369bc356743fba8d42b9fb",
+    "url": "/Foodie-WaitTime-Map/static/media/waiting_1.c4ba9cb1.jpeg"
+  },
+  {
+    "revision": "c9f64a846382b0a4815c2326fbd1994b",
+    "url": "/Foodie-WaitTime-Map/static/media/head1.c9f64a84.jpg"
+  },
+  {
+    "revision": "b34ada23118444907133ce4f17619f4e",
+    "url": "/Foodie-WaitTime-Map/static/media/copyright.b34ada23.jpg"
+  },
+  {
+    "revision": "9a29f817ee95948c93c0",
+    "url": "/Foodie-WaitTime-Map/static/js/runtime~main.9a29f817.js"
+  },
+  {
+    "revision": "3d3376c3c551bb2d72c7",
+    "url": "/Foodie-WaitTime-Map/static/js/main.3d3376c3.chunk.js"
+  },
+  {
+    "revision": "b74a897137c6b364d15b",
+    "url": "/Foodie-WaitTime-Map/static/js/2.b74a8971.chunk.js"
+  },
+  {
+    "revision": "3d3376c3c551bb2d72c7",
+    "url": "/Foodie-WaitTime-Map/static/css/main.9a6f31b5.chunk.css"
+  },
+  {
+    "revision": "b74a897137c6b364d15b",
+    "url": "/Foodie-WaitTime-Map/static/css/2.73fb126a.chunk.css"
+  },
+  {
+    "revision": "b29a4a659cd8b9d90abd9dcdc133a2a6",
+    "url": "/Foodie-WaitTime-Map/index.html"
+  }
+];
